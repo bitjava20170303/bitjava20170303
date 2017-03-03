81be20f27b262edaf5e2b17c5955bc8d894be893
teQoo <anes2354@gmail.com>
2017-03-03 15:07:02 +0900

    coffee!

--- git_share_test01/src/asdfsadf/Dadsfasd.java
+++
@@ -1,9 +0,0 @@
-package asdfsadf;
-
-public class Dadsfasd {
-
-	public static void main(String[] args) {
-		//¿Ö µÇ³Ä
-	}
-
-}
--- git_share_test01/src/bang/BangBron.java
+++
@@ -1,9 +0,0 @@
-package bang;
-
-public class BangBron {
-
-	public static void main(String[] args) {
-		// 2번입니다.
-	}
-
-}
